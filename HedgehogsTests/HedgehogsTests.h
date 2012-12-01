65d20afaa9c595e0db4c43777337381e5ed0d3de
student <student@admins-Mac-mini-3.local>
2012-12-01 13:05:51 +0200

    Initial Commit

---
+++ HedgehogsTests/HedgehogsTests.h
@@ -0,0 +1,13 @@
+//
+//  HedgehogsTests.h
+//  HedgehogsTests
+//
+//  Created by student on 12/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HedgehogsTests : SenTestCase
+
+@end
---
+++ HedgehogsTests/HedgehogsTests.m
@@ -0,0 +1,32 @@
+//
+//  HedgehogsTests.m
+//  HedgehogsTests
+//
+//  Created by student on 12/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HedgehogsTests.h"
+
+@implementation HedgehogsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HedgehogsTests");
+}
+
+@end
